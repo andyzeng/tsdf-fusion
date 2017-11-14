96b531094466bef518ffd7bde4e544e054267c7f
Andy Zeng <andy.zeng.workhorse@gmail.com>
2017-11-13 19:39:48 -0800

    Revert: bug fix: voxel grid origin in tsdf2mesh.m should reflect x-y axes swap.

--- tsdf2mesh.m
+++ tsdf2mesh.m
@@ -26,8 +26,8 @@ faces = fv.faces';
 color = uint8(repmat([175;198;233],1,size(points,2)));
 
 % Transform mesh from voxel coordinates to camera coordinates
-meshPoints(1,:) = voxelGridOrigin(2) + points(2,:)*voxelSize; % x y axes are swapped from isosurface
-meshPoints(2,:) = voxelGridOrigin(1) + points(1,:)*voxelSize;
+meshPoints(1,:) = voxelGridOrigin(1) + points(2,:)*voxelSize; % x y axes are swapped from isosurface
+meshPoints(2,:) = voxelGridOrigin(2) + points(1,:)*voxelSize;
 meshPoints(3,:) = voxelGridOrigin(3) + points(3,:)*voxelSize;
 
 % Write header for mesh file
